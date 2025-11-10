b772c80e9c464d49e620ef8460dc657b28153ffc
Aniket korwar <aniketkorwa@gmail.com>
2025-11-10 20:58:56 +0530

    Fix: Re-enable code preview window - Wrap content with BrowerRunCode component so Run Code button opens preview window

--- src/app/(dashboard)/editor/layout.tsx
+++ src/app/(dashboard)/editor/layout.tsx
@@ -5,6 +5,7 @@ import EditorSidebar from "./_component/EditorSidebar";
 import FileOpen from "./_component/FileOpen";
 import ConnectionStatus from "./_component/ConnectionStatus";
 import ChatPanel from "./_component/ChatPanel";
+import BrowerRunCode from "./_component/BrowerRunCode";
 import { EditorProviderComp } from "./_provider/EditorProvider";
 
 export default function EditorLayout({
@@ -21,30 +22,32 @@ export default function EditorLayout({
         </div>
         
         {/* Main Content Area */}
-        <div className="flex-1 flex overflow-hidden">
-          {/* COMPONENT 1: Left Sidebar - File Explorer */}
-          <aside className="w-64 bg-[#252526] border-r border-[#2d2d2d] flex-shrink-0">
-            <EditorSidebar />
-          </aside>
+        <div className="flex-1 flex overflow-hidden relative">
+          <BrowerRunCode>
+            {/* COMPONENT 1: Left Sidebar - File Explorer */}
+            <aside className="w-64 bg-[#252526] border-r border-[#2d2d2d] flex-shrink-0">
+              <EditorSidebar />
+            </aside>
 
-          {/* COMPONENT 2: Code Editor Area */}
-          <main className="flex-1 flex flex-col overflow-hidden bg-[#1e1e1e]">
-            {/* COMPONENT 3: Tab Bar - shows open file & connection status */}
-            <div className="h-10 bg-[#252526] border-b border-[#2d2d2d] flex items-center justify-between px-2">
-              {/* Left: File Tab */}
-              <div className="flex-1">
-                <FileOpen />
+            {/* COMPONENT 2: Code Editor Area */}
+            <main className="flex-1 flex flex-col overflow-hidden bg-[#1e1e1e]">
+              {/* COMPONENT 3: Tab Bar - shows open file & connection status */}
+              <div className="h-10 bg-[#252526] border-b border-[#2d2d2d] flex items-center justify-between px-2">
+                {/* Left: File Tab */}
+                <div className="flex-1">
+                  <FileOpen />
+                </div>
+                
+                {/* Right: Internet Speed Indicator */}
+                <ConnectionStatus />
               </div>
-              
-              {/* Right: Internet Speed Indicator */}
-              <ConnectionStatus />
-            </div>
 
-            {/* Editor content - takes full remaining space */}
-            <div className="flex-1 overflow-auto bg-[#1e1e1e]">
-              {children}
-            </div>
-          </main>
+              {/* Editor content - takes full remaining space */}
+              <div className="flex-1 overflow-auto bg-[#1e1e1e]">
+                {children}
+              </div>
+            </main>
+          </BrowerRunCode>
         </div>
 
         {/* COMPONENT 4: Floating Chat Panel */}
